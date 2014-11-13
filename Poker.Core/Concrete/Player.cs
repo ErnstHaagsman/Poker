376328be90d54afd93d79038f31366797f74a89f
Ernst Haagsman <ernsthaagsman@gmail.com>
2014-11-13 23:51:09 +0100

    Created Player Class, and added skeleton for PokerGame

---
+++ Poker.Core/Concrete/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Poker.Core;
+using Poker.Core.Interfaces;
+
+namespace Poker.Core.Concrete
+{
+    public class Player : IPlayer
+    {
+        public int Balance { get; set; }
+
+        private int bet;
+        public int Bet
+        {
+            get { return bet; }
+        }
+
+        public string Name { get; set; }
+
+        public PlayerParticipation Participation { get; set; }
+
+        public void Fold()
+        {
+            this.Participation = PlayerParticipation.Inactive;
+        }
+
+        public void Lose()
+        {
+            this.bet = 0;
+        }
+
+        public void Lose(int amount)
+        {
+            if (amount > this.bet)
+                throw new ArgumentOutOfRangeException("amount","Amount larger than bet");
+
+            this.bet -= amount;
+        }
+
+        public void PlaceBet(int amount)
+        {
+            if (amount >= this.Balance)
+            {
+                this.bet = this.Balance;
+                this.Balance = 0;
+                this.Participation = PlayerParticipation.AllIn;
+                return;
+            }
+
+            this.bet += amount;
+            this.Balance -= amount;
+        }
+
+        public void Win(int amount)
+        {
+            this.Balance += this.bet;
+            this.bet = 0;
+            this.Balance += amount;
+        }
+    }
+}
---
+++ Poker.Core/Concrete/PokerGame.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Poker.Core;
+using Poker.Core.Interfaces;
+
+namespace Poker.Core.Concrete
+{
+    class PokerGame : IPokerGame
+    {
+        public int BigBlind
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPlayer BigBlindPlayer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int SmallBlind
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPlayer SmallBlindPlayer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int CurrentBet
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IPlayer CurrentPlayer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public List<IPlayer> Players
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public PokerGameStage Stage
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<PokerGameEventArgs> NextBettingRound;
+
+        public event EventHandler<PokerPlayerEventArgs> NextPlayer;
+
+        public void AddPlayer(IPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemovePlayer(IPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fold()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceBet(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayersWin(List<IPlayer> winners)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayerWins(IPlayer winner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
