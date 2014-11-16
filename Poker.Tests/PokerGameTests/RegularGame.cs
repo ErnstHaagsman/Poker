db45a23361e77ce1445e564e384ff03fcb016e6d
Ernst Haagsman <ernsthaagsman@gmail.com>
2014-11-17 00:04:15 +0100

    Created stub for game test

---
+++ Poker.Tests/PokerGameTests/RegularGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Poker.Core;
+using Poker.Core.Interfaces;
+using Poker.Core.Concrete;
+
+using Xunit;
+
+namespace Poker.Tests.PokerGameTests
+{
+    public class RegularGame
+    {
+        /// <summary>
+        /// Factory method
+        /// </summary>
+        /// <returns></returns>
+        IPokerGame getGame()
+        {
+            return new PokerGame();
+        }
+
+        public RegularGame()
+        {
+        }   
+
+    }
+}
